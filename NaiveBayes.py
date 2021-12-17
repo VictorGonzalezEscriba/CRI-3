aab62a55345fae3c414ba1f621e4b44795be7140
VictorGonzalezEscriba <Xavierfoster18>
2021-12-17 15:44:00 +0100

    last version

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,11 +1,12 @@
 import numpy as np
 from nltk.corpus import stopwords
+from autocorrect import Speller
 from operator import itemgetter
 words = set(stopwords.words('english'))
 
 
 class NaiveBayes:
-    def __init__(self, n_words, stop=False):
+    def __init__(self, n_words, stop=False, correct=False):
         # Probability of positive message
         self.p_prob = 0
         # Probability of negative message
@@ -26,6 +27,10 @@ class NaiveBayes:
         self.laplace = 1
         # Use stopwords or not
         self.stop = stop
+        # To correct the words or not
+        self.correct = correct
+        # To correct the words
+        self.speller = Speller(lang='en')
 
     def stop_words(self):
         for word in words:
@@ -34,28 +39,31 @@ class NaiveBayes:
             if word in self.n_dict:
                 del self.n_dict[word]
 
-    # Generation of the dictionaries
+    # Generation of the dictionaries for positive and negative words
     def fit(self, train):
+        # 0: negative, 1: positive
         p = train[train['sentimentLabel'] == 1]['tweetText'].to_numpy()
         n = train[train['sentimentLabel'] == 0]['tweetText'].to_numpy()
+
+        # Laplace Smoothing
         self.p_prob = (len(p) + self.laplace) / (len(p) + len(n) + (self.laplace*2))
         self.n_prob = (len(n) + self.laplace) / (len(p) + len(n) + (self.laplace*2))
 
         # Positive dictionary
         for i in p:
-            for paraula in i.split(" "):
-                if paraula not in self.p_dict:
-                    self.p_dict[paraula] = 1
+            for word in i.split(" "):
+                if word not in self.p_dict:
+                    self.p_dict[word] = 1
                 else:
-                    self.p_dict[paraula] += 1
+                    self.p_dict[word] += 1
 
         # Negative dictionary
         for i in n:
-            for paraula in i.split(" "):
-                if paraula not in self.n_dict:
-                    self.n_dict[paraula] = 1
+            for word in i.split(" "):
+                if word not in self.n_dict:
+                    self.n_dict[word] = 1
                 else:
-                    self.n_dict[paraula] += 1
+                    self.n_dict[word] += 1
 
         if self.stop:
             self.stop_words()
@@ -66,3 +74,51 @@ class NaiveBayes:
         self.p_sum = sum(self.p_dict.values())
         self.n_sum = sum(self.n_dict.values())
         self.total_dict_words = len(set(self.p_dict.keys()) | set(self.n_dict.keys()))
+
+    def predict(self, test):
+        twtstest = test['tweetText'].to_numpy()
+        pred = np.zeros(len(twtstest), dtype=int)
+        if self.stop:
+            for i in range(0, len(twtstest)):
+                prob_pos = 1
+                prob_neg = 1
+                for paraula in twtstest[i].split(" "):
+                    if paraula not in words:
+                        if paraula in self.p_dict:
+                            prob_pos *= ((self.p_dict[paraula] + self.laplace) / (
+                                        self.p_sum + (self.total_dict_words * self.laplace)))
+                        # We exclude words with a length less than 3
+                        if paraula not in self.p_dict and len(paraula) > 2:
+                            prob_pos *= ((0 + self.laplace) / (self.p_sum + (self.total_dict_words * self.laplace)))
+
+                        if paraula in self.n_dict:
+                            prob_neg *= ((self.n_dict[paraula] + self.laplace) / (
+                                        self.n_sum + (self.total_dict_words * self.laplace)))
+                        # We exclude words with a length less than 3
+                        if paraula not in self.n_dict and len(paraula) > 2:
+                            prob_neg *= ((0 + self.laplace) / (self.n_sum + (self.total_dict_words * self.laplace)))
+                prob_pos *= self.p_prob
+                prob_neg *= self.n_prob
+                pred[i] = int(prob_pos > prob_neg)
+        else:
+            for i in range(0, len(twtstest)):
+                prob_pos = 1
+                prob_neg = 1
+                for paraula in twtstest[i].split(" "):
+                    if paraula not in words:
+                        if paraula in self.p_dict:
+                            prob_pos *= ((self.p_dict[paraula] + self.laplace) / (
+                                        self.p_sum + (self.total_dict_words * self.laplace)))
+                        # We exclude words with a length less than 3
+                        if paraula not in self.p_dict and len(paraula) > 2:
+                            prob_pos *= ((0 + self.laplace) / (self.p_sum + (self.total_dict_words * self.laplace)))
+                        if paraula in self.n_dict:
+                            prob_neg *= ((self.n_dict[paraula] + self.laplace) / (
+                                        self.n_sum + (self.total_dict_words * self.laplace)))
+                        # We exclude words with a length less than 3
+                        if paraula not in self.n_dict and len(paraula) > 2:
+                            prob_neg *= ((0 + self.laplace) / (self.n_sum + (self.total_dict_words * self.laplace)))
+                prob_pos *= self.p_prob
+                prob_neg *= self.n_prob
+                pred[i] = int(prob_pos > prob_neg)
+        return pred
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from NaiveBayes import NaiveBayes
 
 # Function to read data
 def load_dataset(path):
-    df = pd.read_csv(path, header=0, delimiter=', ')
+    df = pd.read_csv(path, header=0, delimiter=';')
     return df
 
 
@@ -21,3 +21,164 @@ def split_data(data, train_ratio=0.7):
     train = pd.concat([p[m_p], n[m_n]])
     test = pd.concat([p[~m_p], n[~m_n]])
     return train, test
+
+
+# To calculate the "confusion matrix"
+def metrics(y_true, pred):
+    conf_matrix = np.array([0.0, 0.0, 0.0, 0.0])  # true_positive, false_positive, false_negative, true_negative
+    if len(y_true) == len(pred):
+        y_true_neg = np.where((y_true == 0) | (y_true == 1), y_true ^ 1, y_true)
+        pred_n = np.where((pred == 0) | (pred == 1), pred ^ 1, pred)
+        c0 = np.count_nonzero(np.logical_and(pred, y_true))
+        c1 = np.count_nonzero(np.logical_and(pred, y_true_neg))
+        c2 = np.count_nonzero(np.logical_and(pred_n, y_true))
+        c3 = np.count_nonzero(np.logical_and(pred_n, y_true_neg))
+        conf_matrix[0] += c0 / (c0 + c1)
+        conf_matrix[1] += c1 / (c0 + c1)
+        conf_matrix[2] += c2 / (c2 + c3)
+        conf_matrix[3] += c3 / (c2 + c3)
+        return conf_matrix, (np.count_nonzero(y_true == pred)/len(pred))
+    return conf_matrix, 0
+
+
+# K-fold Cross Validation
+def cross_validation(data, k=5, words=2500, stop=False):
+    mean_accuracy = 0.0
+    mean_confidence = np.array([0.0, 0.0, 0.0, 0.0])
+    shuffled = data.sample(frac=1)
+    partitions = np.array_split(shuffled, k)
+
+    for i in range(0, k):
+        train = []
+        for j in range(0, k):
+            if j != i:
+                train.append(partitions[j])
+        train = pd.concat(train)
+        test = partitions[i]
+        nb = NaiveBayes(words, stop=stop, correct=True)
+        nb.fit(train)
+        prediction = nb.predict(test)
+        conf_matrix, acc = metrics(test['sentimentLabel'].to_numpy(), prediction)
+        mean_accuracy += acc
+        mean_confidence += conf_matrix
+    return (mean_confidence / k), (mean_accuracy / k)
+
+
+def main():
+    csv = 'FinalStemmedSentimentAnalysisDataset.csv'
+    print("Reading file:" + csv)
+    tweets = load_dataset(csv)
+    print("\n===== Data =====")
+    print("- Total number of samples: " + str(tweets.shape[0]))
+    print("- Number of attributes: " + str(tweets.shape[1]))
+    print("\n- Percentage of positive tweets: " + str(round((tweets[tweets['sentimentLabel'] == 1].shape[0] / tweets.shape[0]) * 100, 3)))
+    print("- Percentage of negative tweets: " + str(round((tweets[tweets['sentimentLabel'] == 0].shape[0] / tweets.shape[0]) * 100, 3)))
+    print("\n===== Cleaning NaN values =====")
+    tweets = tweets.dropna()
+    print("- Total number of samples: " + str(tweets.shape[0]))
+    print("- Number of attributes " + str(tweets.shape[1]))
+    print("\n- Percentage of positive tweets: " + str(round((tweets[tweets['sentimentLabel'] == 1].shape[0] / tweets.shape[0]) * 100, 3)))
+    print("- Percentage of negative tweets: " + str(round((tweets[tweets['sentimentLabel'] == 0].shape[0] / tweets.shape[0]) * 100, 3)))
+
+    print("\n===== Models =====")
+    print("- Naive bayes without removing stopwords:")
+    train, test = split_data(tweets)
+    print("\tTraining model  ...", end="")
+    start = time.time()
+    nb = NaiveBayes(2500, correct=True)
+    nb.fit(train)
+    end = time.time()
+    temps1 = round(end - start, 4)
+    print("\t" + str(temps1) + " seconds")
+    print("\tModel prediction ...", end="")
+    start = time.time()
+    pred = nb.predict(test)
+    end = time.time()
+    temps2 = round(end - start, 4)
+    print("\t" + str(temps2) + " seconds")
+    print("\t-----------------------------------------------")
+    print("\tTotal time: " + str(temps1 + temps2) + " seconds")
+    print("\t-----------------------------------------------")
+    confm, acc = metrics(test['sentimentLabel'].to_numpy(), pred)
+    print("\tAccuracy: " + str(round(acc * 100, 3)))
+    print("\tTruePositives: " + str(round(confm[0] * 100, 3)) + "\tFalsePositives: " + str(round(confm[1] * 100, 3)))
+    print("\tFalseNegatives: " + str(round(confm[2] * 100, 3)) + "\tTrueNegatives: " + str(round(confm[3] * 100, 3)))
+
+    print("\n- Naive bayes removing stopwords:")
+    print("\tTraining model  ...", end="")
+    start = time.time()
+    nb = NaiveBayes(2500, stop=True, correct=True)
+    nb.fit(train)
+    end = time.time()
+    temps1 = round(end - start, 4)
+    print("\t" + str(temps1) + " seconds")
+    print("\tModel prediction ...", end="")
+    start = time.time()
+    pred = nb.predict(test)
+    end = time.time()
+    temps2 = round(end - start, 4)
+    print("\t" + str(temps2) + " seconds")
+    print("\t-----------------------------------------------")
+    print("\tTotal time: " + str(temps1 + temps2) + " seconds")
+    print("\t-----------------------------------------------")
+    confm, acc = metrics(test['sentimentLabel'].to_numpy(), pred)
+    print("\tAccuracy: " + str(round(acc * 100, 3)))
+    print("\tTruePositives: " + str(round(confm[0] * 100, 3)) + "\tFalsePositives: " + str(round(confm[1] * 100, 3)))
+    print("\tFalseNegatives: " + str(round(confm[2] * 100, 3)) + "\tTrueNegatives: " + str(round(confm[3] * 100, 3)))
+
+    print("\n- Effect of train-test set size:")
+    part = [0.5, 0.6, 0.7, 0.8]
+    for i in range(0, len(part)):
+        print("\n\t" + str(part[i] * 100) + "-" + str(100 - (part[i] * 100)) + ":")
+        train, test = split_data(tweets, train_ratio=part[i])
+        print("\tTraining model  ...", end="")
+        start = time.time()
+        nb = NaiveBayes(2500, stop=True, correct=True)
+        nb.fit(train)
+        end = time.time()
+        temps1 = round(end - start, 4)
+        print("\t" + str(temps1) + " seconds")
+        print("\tModel prediction ...", end="")
+        start = time.time()
+        pred = nb.predict(test)
+        end = time.time()
+        temps2 = round(end - start, 4)
+        print("\t" + str(temps2) + " seconds")
+        print("\t-----------------------------------------------")
+        print("\tTotal time: " + str(temps1 + temps2) + " seconds")
+        print("\t-----------------------------------------------")
+        confm, acc = metrics(test['sentimentLabel'].to_numpy(), pred)
+        print("\tAccuracy: " + str(round(acc * 100, 3)))
+        print("\tTruePositives: " + str(round(confm[0] * 100, 3)) + "\tFalsePositives: " + str(round(confm[1] * 100, 3)))
+        print("\tFalseNegatives: " + str(round(confm[2] * 100, 3)) + "\tTrueNegatives: " + str(round(confm[3] * 100, 3)))
+
+    print("\n- Changing the size of dictionaries with K-fold K = 5.")
+    paraules = [500, 2000, 3500, 5000, 6500, 8000, 0]
+    for i in range(0, len(paraules)):
+        if paraules[i] > 0:
+            print("\n\t" + str(paraules[i]) + " words:")
+        else:
+            print("\n\tAll words:")
+        print("\tCalculating ...", end="")
+        start = time.time()
+        confm, acc = cross_validation(tweets, 5, words=paraules[i], stop=True)
+        end = time.time()
+        temps2 = round(end - start, 4)
+        print("\t" + str(temps2) + " seconds")
+        print("\tAccuracy: " + str(round(acc * 100, 3)))
+        print("\tTruePositives: " + str(round(confm[0] * 100, 3)) + "\tFalsePositives: " + str(round(confm[1] * 100, 3)))
+        print("\tFalseNegatives: " + str(round(confm[2] * 100, 3)) + "\tTrueNegatives: " + str(round(confm[3] * 100, 3)))
+
+
+# If you want to re-write the .txt file, set "p = True". It takes 15 min to finish.
+p = True
+if p:
+    original_stdout = sys.stdout
+    # Creates a txt file to store the result, don't show anything by console
+    with open('NaiveBayesOutputCorrect.txt', 'w') as f:
+        sys.stdout = f
+        main()
+        sys.stdout = original_stdout
+    print("Check NaiveBayesOutput.txt")
+else:
+    main()
